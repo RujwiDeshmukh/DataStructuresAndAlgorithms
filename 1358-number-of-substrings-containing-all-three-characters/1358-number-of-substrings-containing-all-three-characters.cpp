45e865c2d8cf2c1d21bdd0d0d6b7f779c21f225a
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-01-23 17:21:58 +0530

    Time: 87 ms (25.24%), Space: 14.2 MB (10.07%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int numberOfSubstrings(string s) {
+        //variable sliding window technique
+
+        unordered_map<char,int>mp;
+        int n = s.size();
+        
+        int end=0 , start=0;
+        int total=0;
+        
+        while(end < s.size())
+        {
+            mp[s[end]]++;
+            
+            if(mp.size() < 3)
+            {
+                end++;
+                continue;
+            }
+            
+            while(mp.size() == 3)
+            {
+                 total+= (n-end);
+                 mp[s[start]]--;
+                 if(mp[s[start]]==0)
+                 {
+                     mp.erase(s[start]);
+                 }
+                start++;
+            }
+            
+            end++;
+        }
+        
+        return total;
+    }
+};
