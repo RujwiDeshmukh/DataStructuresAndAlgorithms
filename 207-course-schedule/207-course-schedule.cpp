fb3095b32683ec7eaecf565cf679225bed3e8076
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-02-10 22:45:33 +0530

    Time: 24 ms (68.40%), Space: 15.8 MB (12.58%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    
+    bool isCyclic(vector<int>adj[],int curr,unordered_set<int>& vis,unordered_set<int>& dfs)
+    {
+        vis.insert(curr);
+        dfs.insert(curr);
+        
+        for(auto neigh : adj[curr])
+        {
+        
+        if(vis.find(neigh)==vis.end())
+        {
+            if(isCyclic(adj,neigh,vis,dfs))
+            {
+                return true;
+            }
+        }
+            
+            else if(dfs.find(neigh) != dfs.end())
+            {
+                return true;
+            }
+            
+        }
+        
+        dfs.erase(curr);
+        return false;
+        
+    }
+    
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        
+        vector<int>adj[n];
+        
+        //graph created 
+        for(int i=0;i<edges.size();i++)
+        {
+            vector<int>temp = edges[i];
+            
+            adj[temp[1]].push_back(temp[0]);
+        }
+        
+        unordered_set<int>vis;
+        unordered_set<int>dfs;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(vis.find(i)==vis.end())
+            {
+                if(isCyclic(adj,i,vis,dfs))
+                {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+        
+    }
+};
