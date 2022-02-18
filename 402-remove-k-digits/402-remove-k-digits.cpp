d06898b700a762513aec25c07fc6dd0a4f459c29
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-02-18 14:51:36 +0530

    Time: 71 ms (12.13%), Space: 6.8 MB (95.64%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+       
+        //we will choose the peak to remove it so
+        //we will remove such k elements
+
+        while(k > 0)
+        {
+            //for the increasing curve, once the decrease curve is detected
+            //halt the loop
+            int i=0;
+            while(i+1< num.size() && num[i]<=num[i+1])
+            {
+                i++;
+            }
+            //here as we are stating from 1 index i.e. i=1 so
+            //i will increment first so we have to erase i-1
+            num.erase(i,1);
+            k--;
+        }
+        
+        //checking for the leading zeros
+        
+        cout<<num.size()<<endl;
+        
+        int i=0;
+        
+        while(i<num.size()-1 && num[i]=='0')
+        {
+            i++;
+        }
+        //first arg is index and second one is length of number of zeros
+        num.erase(0,i);
+        
+        return num=="" ? "0" : num;
+    }
+};
