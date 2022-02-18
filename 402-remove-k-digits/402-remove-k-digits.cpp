cf16d77db0c99220878902bf294a8ba2bfa13810
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-02-18 15:25:45 +0530

    Time: 15 ms (25.13%), Space: 7.2 MB (28.19%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,38 +1,56 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-       
-        //we will choose the peak to remove it so
-        //we will remove such k elements
-
-        while(k > 0)
+        
+        stack<char>st;
+        string ans="";
+        
+        if(num.size()==k)
         {
-            //for the increasing curve, once the decrease curve is detected
-            //halt the loop
-            int i=0;
-            while(i+1< num.size() && num[i]<=num[i+1])
-            {
-                i++;
-            }
-            //here as we are stating from 1 index i.e. i=1 so
-            //i will increment first so we have to erase i-1
-            cout<<i<<endl;
-            num.erase(i,1);
-            k--;
+            return "0";
         }
-        
-        //checking for the leading zeros
-        
         
         int i=0;
-        
-        while(i<num.size()-1 && num[i]=='0')
+        while(i< num.size())
         {
+            while(!st.empty() && st.top() > num[i] && k)
+            {
+                st.pop();
+                k--;
+            }
+            
+            if(num[i] == '0' )
+            { 
+                if(!st.empty())
+                {
+                st.push(num[i]);
+                }
+            }
+            else
+            {
+                st.push(num[i]);
+            }
+            
             i++;
         }
-        //first arg is index and second one is length of number of zeros
-        num.erase(0,i);
         
-        return num=="" ? "0" : num;
+        while(k>0 && !st.empty())
+        {
+            k--;
+            cout<<st.top()<<endl;
+            st.pop();
+            
+        }
+        
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        
+        reverse(ans.begin(),ans.end());
+        
+        return ans=="" ? "0" : ans;;
+        
     }
 };
