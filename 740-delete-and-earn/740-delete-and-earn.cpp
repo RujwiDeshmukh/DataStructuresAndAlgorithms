db51d60b799685402b3a954b7e9718f15cc2c804
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-02-19 21:21:05 +0530

    Time: 12 ms (51.92%), Space: 11.9 MB (27.14%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int deleteEarn(vector<int>& count,int idx,int dp[])
+    {
+        if(idx >= count.size())
+        {
+            return 0;
+        }
+        
+        if(dp[idx] != -1)
+        {
+            return dp[idx];
+        }
+        
+        int consider = count[idx]+deleteEarn(count,idx+2,dp);
+        int notConsider = deleteEarn(count,idx+1,dp);
+        
+        return dp[idx] = max(notConsider,consider);
+    }
+    
+    int deleteAndEarn(vector<int>& nums) {
+        
+        vector<int>count(10001,0);
+        int dp[10001];
+        
+        memset(dp,-1,sizeof(dp));
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            count[nums[i]] += nums[i];
+        }
+        
+        return deleteEarn(count,0,dp);
+    }
+};
