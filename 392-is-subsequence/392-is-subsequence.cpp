bff9c936b4d2c9a01d6f3bcbd944abe149007e33
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-03-02 23:09:16 +0530

    Time: 8 ms (15.48%), Space: 6.5 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n = s.length(),m=t.length();
-        int j = 0; 
-    // For index of s (or subsequence
- 
-    // Traverse s and t, and
-    // compare current character
-    // of s with first unmatched char
-    // of t, if matched
-    // then move ahead in s
-    for (int i = 0; i < m and j < n; i++)
-        if (s[j] == t[i])
-            j++;
- 
-    // If all characters of s were found in t
-    return (j == n);
+     
+         int m = s.length();
+         int n = t.length();
+        
+        int j=0;
+        
+        for(int i=0; j<m && i<n;i++)
+        {
+            //increment only if the matching character is found
+            if(s[j]==t[i])
+            {
+                j++;
+            }
+        }
+        
+        //check the pointer is equal to j
+        return (j==m);
     }
 };
