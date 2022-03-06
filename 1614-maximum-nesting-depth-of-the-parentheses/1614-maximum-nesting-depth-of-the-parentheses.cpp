cd59967100e5a04aed3bb5a756c9113a0b0dcd91
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-03-06 11:26:11 +0530

    Time: 2 ms (42.05%), Space: 6.4 MB (20.89%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     int maxDepth(string s) {
         
-         /*
-        if we read the que correctly, all vps are valid parenthesis that is equal closing and opening bracket
-        que is input is a valid parwnthesis, we just need to count depth
-        so depth of any char is no of open paranthesis till we reach that char
+        int n = s.length();
+        stack<char>st;
+        int maxi=0;
         
-        so we could use stack for this count or to prevent space directly count the open paranthesis
-        
-        so max no of open parenthesis is ans.
-        */
-        
-        int open=0;
-        int res=0;
-        
-        for(int i=0;i<s.length();i++)
+        for(int i=0;i<n;i++)
         {
             if(s[i]=='(')
             {
-                open++;
-                res = max(res,open);
+                st.push(s[i]);
             }
             else if(s[i]==')')
             {
-                open--;
+                int size = st.size();
+                maxi = max(maxi,size);
+                st.pop();
             }
         }
         
-        return res;
+        return maxi;
     }
 };
