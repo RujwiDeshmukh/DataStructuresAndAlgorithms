f945f326784075887cc931c4884497509396e9bb
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-03-06 17:52:36 +0530

    Time: 10 ms (99.01%), Space: 11 MB (67.92%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string res = "";
-        for (char& c : s)
-            if (res.size() && c == res.back())
+        
+        string res="";
+        
+        for(char &ch : s)
+        {
+            if(res.size() && res.back()==ch )
+            {
                 res.pop_back();
+            }
             else
-                res.push_back(c);
+            {
+                res.push_back(ch);
+            }
+        }
+        
         return res;
     }
 };
