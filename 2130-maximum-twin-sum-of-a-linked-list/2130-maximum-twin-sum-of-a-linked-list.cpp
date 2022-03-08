fcf7616f51cee9445e7d39c19f6e225a9a5a7831
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-03-08 16:42:39 +0530

    Time: 439 ms (33.70%), Space: 122.1 MB (56.89%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,53 +10,28 @@
  */
 class Solution {
 public:
-    
-    ListNode* middleNode(ListNode* head)
-    {
-        ListNode *slow = head;
-        ListNode *fast = head;
-        
-        while(fast->next != NULL && fast->next->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        
-        cout<<slow->val<<endl;
-        return slow;
-    }
-    
-    ListNode* reverseList(ListNode* head)
-    {
-        ListNode* prev=NULL;
-
-        while(head != NULL)
-        {
-            ListNode* nextNode = head->next;
-            head->next = prev;
-            prev = head;
-            head= nextNode;
-        }
-        
-        return prev;
-    }
-    
     int pairSum(ListNode* head) {
         
-        ListNode* mid = middleNode(head);
-        cout<<mid->val<<endl;
-        ListNode* midNext = mid->next;
-        cout<<midNext->val<<endl;
-        int maxValue=0;
-        mid->next=NULL;
+        ListNode* curr = head;
+        stack<int>st;
         
-        midNext = reverseList(midNext);
-        cout<<midNext->val<<endl;
-        
-        while(midNext != NULL)
+        while(curr != NULL)
         {
-           maxValue = max(maxValue,(midNext->val + head->val));
-           midNext = midNext->next;
-           head = head->next;
+           st.push(curr->val);
+           curr=curr->next;
+        }
+        
+        curr=head;
+        int count=0;
+        int size=st.size();
+        int maxValue=0;
+        
+        while(count < (size)/2)
+        {
+            maxValue = max(maxValue, st.top()+curr->val);
+            st.pop();
+            curr=curr->next;
+            count++;
         }
         
         return maxValue;
