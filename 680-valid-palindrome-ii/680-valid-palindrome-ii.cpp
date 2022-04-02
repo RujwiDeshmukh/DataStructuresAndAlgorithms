3392b67def0e57177666cb7f4b281494edb64093
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-04-02 17:42:52 +0530

    Time: 76 ms (37.15%), Space: 24.1 MB (31.50%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    
+    bool isPalindrome(string s,int l ,int r)
+    {
+        while(l<r)
+        {
+            if(s[l] != s[r])
+            {
+                return false;
+            }
+            
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+    
+    
+    bool validPalindrome(string s) {
+        
+        int l = 0;
+        int r = s.length()-1;
+        
+        while(l<r)
+        {
+            if(s[l] != s[r])
+            {
+                if(isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+            l++;
+            r--;
+        }
+        
+        return true;
+    }
+};
