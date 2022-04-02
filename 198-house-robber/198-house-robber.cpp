86740cd28fa09fbda61cea53dfebc90b49bf700c
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-04-02 20:09:38 +0530

    Time: 2 ms (53.98%), Space: 7.6 MB (76.05%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,22 +4,31 @@ public:
      
         int n = nums.size();
         
-        int dp[n+1];
+        //int dp[n+1];
         
         //we got to know that from recursive tree
-        dp[0] = nums[0];
+        //rather than using dp array use variables to reduce the space complexity
+        //dp[0] = nums[0];
         int pick=0;
+        
+        int prev = nums[0];
+        int prev2 = 0;
         
         for(int i=1;i<n;i++)
         {
-         i > 1 ? pick = nums[i] + dp[i-2] : pick = nums[i]; //+ dp[i-2] ll be 0 as i ind is -ve;
+         i > 1 ? pick = nums[i] + prev2 : pick = nums[i]; //+ dp[i-2] ll be 0 as i ind is -ve;
         
-          int notPick = dp[i-1];
+          int notPick = prev;
             
-         dp[i] = max(pick,notPick);
+         int curr = max(pick,notPick);
+            
+         prev2 = prev;
+            
+         prev = curr;
+            
         }
         
-        return dp[n-1];
+        return prev;
         
     }
 };
