1028a6be8c9c8402256cfa59b0bdda4322aa681b
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-04-13 21:16:00 +0530

    Time: 182 ms (20.48%), Space: 14.4 MB (46.45%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -56,7 +56,63 @@ public:
         
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return maxCherries(grid,n,m,0,0,m-1,dp);
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,0)));
+        
+        //base condition
+        for(int j1=0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++)
+            {
+                if(j1==j2)
+                {
+                    dp[n-1][j1][j2] = grid[n-1][j1];
+                }
+                else
+                {
+                    dp[n-1][j1][j2] = grid[n-1][j1]+grid[n-1][j2];
+                }
+            }
+        }
+        
+        //as we know the base case is n-1, the looping will start from n-2 position
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j1=0;j1<m;j1++)
+            {
+                for(int j2=0;j2<m;j2++)
+                {
+                      
+                     int maxi = -1e9+8;
+        for(int dj1=-1;dj1<=1;dj1++)
+        {
+            for(int dj2=-1;dj2<=1;dj2++)
+            {
+                int value=0;
+                if(j1==j2)
+                {       
+                    //maxi = max(maxi, grid[r][c1]+maxCherries(grid,n,m,r+1,c1+dj1,c2+dj2));
+                    value += grid[i][j1];
+                }
+                else
+                {
+         //maxi = max(maxi, grid[r][c1]+grid[r][c2]+maxCherries(grid,n,m,r+1,c1+dj1,c2+dj2));
+                    value += grid[i][j1]+grid[i][j2];
+                }
+                
+                if(j1+dj1 >=0 && j1+dj1 < m && j2+dj2 >= 0 && j2+dj2 < m)
+                   value += dp[i+1][j1+dj1][j2+dj2];
+                else
+                    value = -1e8+9;
+                maxi = max(maxi,value);
+            }
+        }
+                    
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        
+        return dp[0][0][m-1];
     }
 };
