90dd1ddb521bba37c2385c7ec43f932083f6ef33
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-04-14 18:10:01 +0530

    Time: 6 ms (36.72%), Space: 7.7 MB (55.87%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
     
-    void recurPermutate(vector<int>& nums,int n,vector<int>& ds,vector<vector<int>>& ans, vector<int>& freq)
+    void recurPermutate(int ind, vector<int>& nums,vector<vector<int>>& ans)
     {
-        if(ds.size() >= n)
+        if(ind >= nums.size())
         {
-            ans.push_back(ds);
+            ans.push_back(nums);
             return;
         }
         
-        //check for all the possibilities that we can pick
+        //for placing that particular element at every possible state 
         
-        for(int i=0;i<n;i++)
+        for(int i=ind;i<nums.size();i++)
         {
-            if(!freq[i])
-            {
-                //mark the index as visited or not from that we can infer if the element
-                //of that same index will be picked or not next time
-                freq[i]=1;
-                ds.push_back(nums[i]);
-                //n! complexity for generating all the permutations of the given array
-                //O(n) complexity => for loop
-                recurPermutate(nums,n,ds,ans,freq);
-                //after the completion of the function call remove it from
-                //data structure and unmark it as well
-                ds.pop_back();
-                freq[i]=0;
-            }
+            //modifying the original array
+            swap(nums[ind],nums[i]);
+            recurPermutate(ind+1,nums,ans);
+            //restoring the swapped values to original array
+            swap(nums[ind],nums[i]);
         }
-        
     }
     
+    
     vector<vector<int>> permute(vector<int>& nums) {
-       vector<vector<int>>ans;
-        int n = nums.size();
-        vector<int>freq(n,0);
-        vector<int>ds;
-        recurPermutate(nums,n,ds,ans,freq);
+         vector<vector<int>>ans;
+        recurPermutate(0,nums,ans);
         return ans;
-        
     }
 };
