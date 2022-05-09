d6ea9c638608ae4eb95a4044215a5d6ec1ecc6a2
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-05-09 17:45:05 +0530

    Time: 167 ms (79.54%), Space: 68 MB (40.23%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -3,7 +3,7 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         
         //efficient appraoch that works in O(n) --->TC
-        
+       // https://leetcode.com/problems/max-number-of-k-sum-pairs/discuss/2005922/Going-from-O(N2)-greater-O(NlogN)-greater-O(N)-%2B-MEME
         unordered_map<int,int>mp;
         int count=0;
         
