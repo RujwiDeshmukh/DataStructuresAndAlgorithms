e36709f69ff4f055dcc1cf0c04f18a4bd1a4c6c9
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-05-14 22:58:06 +0530

    Time: 424 ms (6.73%), Space: 43.5 MB (35.11%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,53 +1,59 @@
 class Solution {
 public:
     
-    int minTime(vector<pair<int,int>> graph[], int src,vector<int>& dist)
+    int minTime(vector<pair<int,int>>graph[],vector<int>& dist,int src)
     {
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,src});
-        //src distance should be zero from itself
         dist[src]=0;
         int ans=0;
         
+        pq.push({0,src});
+        //distance and source
         while(!pq.empty())
         {
             auto pr = pq.top();
             pq.pop();
-            
+            int dis = pr.first;
             int node = pr.second;
+            
+            cout<<node<<endl;
             
             for(auto neigh : graph[node])
             {
-                int nextNode = neigh.first;
                 int nextDist = neigh.second;
+                int nextDest = neigh.first;
+                cout<<nextDist<<endl;
                 
-                if(dist[nextNode] > dist[node]+nextDist)
+                if(dist[nextDest] > dist[node]+nextDist)
                 {
-                    dist[nextNode] = dist[node]+nextDist;
-                    pq.push({dist[nextNode],nextNode});
+                    dist[nextDest] = dist[node]+nextDist;
+                    pq.push({dist[nextDest], nextDest});
                 }
             }
         }
-        
-         ans = *max_element(dist.begin()+1,dist.end());
-        
-        return ans==INT_MAX ? -1 : ans;
+            
+            ans = *max_element(dist.begin()+1,dist.end());
+            
+         return ans == INT_MAX ? -1 : ans;
     }
+    
     
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
         vector<pair<int,int>>graph[n+1];
         vector<int>dist(n+1,INT_MAX);
+        
         for(int i=0;i<times.size();i++)
         {
             vector<int>temp = times[i];
             int src = temp[0];
             int dest = temp[1];
-            int wt = temp[2];
+            int dis = temp[2];
             
-            graph[src].push_back({dest,wt});
+            graph[src].push_back({dest,dis});
         }
         
-       return minTime(graph,k,dist);
+        return minTime(graph,dist,k);
+        
     }
 };
