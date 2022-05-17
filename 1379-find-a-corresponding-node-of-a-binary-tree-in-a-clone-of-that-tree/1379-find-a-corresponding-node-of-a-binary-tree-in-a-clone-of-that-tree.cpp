23c51698012ffc05a64d9bded0892cd02ffe6089
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-05-17 21:06:23 +0530

    Time: 794 ms (34.01%), Space: 164 MB (22.94%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,33 +10,25 @@
 
 class Solution {
 public:
-    
-    
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
         
-        if(original==NULL)
+        if(original == NULL)
         {
             return NULL;
         }
         
-        if(original==target)
+        if(original == target)
         {
             return cloned;
         }
-       
-        //if we want to choose from any of the two branches then 
-        //first we will compute value and will if else condition
-
-    TreeNode* left =  getTargetCopy(original->left,cloned->left,target);
-        //here itself we can check that from left if we get target value
-        //then direct return dont go to right part
-    TreeNode* right =  getTargetCopy(original->right,cloned->right,target);
         
-        if(left!=NULL)
+        TreeNode* left = getTargetCopy(original->left,cloned->left,target);
+        TreeNode* right = getTargetCopy(original->right,cloned->right,target);
+        
+        if(left != NULL)
         {
             return left;
         }
-        
-        return right;
+         return right;
     }
 };
