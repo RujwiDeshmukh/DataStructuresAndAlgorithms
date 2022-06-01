a254840aa45e65f0e108f8211b7431a1fefa7a6c
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-06-01 22:42:52 +0530

    Time: 9 ms (29.51%), Space: 7.3 MB (38.37%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -6,6 +6,10 @@ public:
         st.push(-1);
         int MAX=0;
         
+        //https://leetcode.com/problems/longest-valid-parentheses/discuss/1139990/Longest-Valid-Parentheses-or-Short-and-Easy-w-Explanation-using-stack
+        
+        //https://leetcode.com/problems/longest-valid-parentheses/discuss/864061/C%2B%2B-O(n)-time-and-O(1)-space-(0ms-runtime-beats-100)-explained
+        
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='(')
