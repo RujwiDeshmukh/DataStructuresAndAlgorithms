62320901f1c8ec6d3cc33a09b9567a5086d16582
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-06-10 18:25:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.27%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -2,29 +2,18 @@ class Solution {
 public:
     int brokenCalc(int startValue, int target) {
         
-        int result=0;
+       // https://leetcode.com/problems/broken-calculator/discuss/1874851/oror-C%2B%2B-oror-Easy-Approach-oror-PROPER-EXPLANATION
         
-        while(startValue < target)
+        if(startValue >= target)
         {
-            //entering while loop means performing 1 operation already
-            //so incrementing the result
-            result++;
-            cout<<result<<endl;
-            if(target%2 == 0)
-            {
-                target = target/2;
-            }
-            else
-            {
-                target = target+1;
-            }
+            return startValue - target;
         }
         
-        //now startValue has become greater than target 
-        //then we have to increment target by adding 1 to it everytime
-        //so calculating the difference between target and startValue
-        result = result+(startValue-target);
+        if(target%2==0)
+        {
+            return 1 + brokenCalc(startValue, target/2);
+        }
         
-        return result;
+        return 1 + brokenCalc(startValue , target+1);
     }
 };
