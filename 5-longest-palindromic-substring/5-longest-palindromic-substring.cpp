f97dd3522bf8d604ff0b6d2d58861b6f61ea0150
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-06-17 11:41:31 +0530

    Time: 26 ms (85.78%), Space: 7.1 MB (81.61%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
+
         int n = s.length();
-        int max_length = 1;
         int low,high;
+        string ans="";
         int start=0;
-        string ans;
+        int max_length=1;
+        int len=0;
         
         for(int i=1;i<n;i++)
         {
-            low=i-1;
-            high=i;
-            
-            //even palindrome
-            while(low>=0 && high<n && (s[low]==s[high]))
-            {
-                int len = high-low+1;
+            //check for even palindrome
+            low = i-1;
+            high = i;
+        while(low >= 0 && high<n && s[low]==s[high])
+          {
+                len = high-low+1;
                 
-                if(len>max_length)
+            if(len >  max_length)
+            {
+                max_length = len;
+                start=low;
+            }
+            
+            low--;
+            high++;
+          }
+            
+            //check for odd palindrome
+            
+            low = i-1;
+            high = i+1;
+            
+         while(low >= 0 && high<n && s[low]==s[high])
+            {
+                len = high-low+1;
+               
+                if(len > max_length)
                 {
-                    max_length=len;
+                    max_length = len;
                     start=low;
                 }
-                
                 low--;
                 high++;
             }
-            
-            //odd plaindrome
-            low=i-1;
-            high=i+1;
-            while(low>=0 && high<n && (s[low]==s[high]))
-            {
-                int len=high-low+1;
-                
-                if(len>max_length)
-                {
-                    start=low;
-                    max_length=len;
-                }
-                
-                low--;
-                high++;
-            }
-            
         }
         
-        int end = start+max_length;
-        
+        int end = max_length+start;
         for(int i=start;i<end;i++)
         {
             ans += s[i];
         }
+        
         
         return ans;
     }
