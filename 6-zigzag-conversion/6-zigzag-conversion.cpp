4cfbdb08773e03f6765d873d90aa25c5754f2aa4
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-07-06 22:40:23 +0530

    Time: 22 ms (39.95%), Space: 10.8 MB (40.09%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     string convert(string s, int numRows) {
              
+       // https://leetcode.com/problems/zigzag-conversion/discuss/920510/c%2B%2B(8ms-90)-very-easy-for-understand-(with-commentaries)
+             
         vector<string> tmp(numRows);
-        
         int n = s.size();
-        
-        string ans;
-        
+        string ans; 
         int i=0;
         
         while(i<n)
@@ -15,6 +14,7 @@ public:
            for(int j=0;j<numRows && i<n;j++,i++)
            {
                tmp[j] += s[i];
+               //cannot push_back here as we have already initialized the array
            }
             
            for(int j=numRows-2;j>0 && i<n;j--,i++)
