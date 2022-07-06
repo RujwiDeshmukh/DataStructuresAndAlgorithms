5f81b3b36f9d5cf357bf9e1f29b1bd6be65b7721
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-07-06 19:58:44 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,12 +2,24 @@ class Solution {
 public:
     int fib(int n) {
         
+          int x0=0, x1=1;
+          int x2=0;
+          int temp;
+          int sum=0;
+        
           if(n <= 1)
           {
               return n;
           }
         
-        return fib(n-1)+fib(n-2);
+          for(int i=0;i<n-1;i++)
+          {
+              x2 = x0+x1;
+              temp=x1;
+              x1=x2;
+              x0=temp;
+          }
         
+          return x2;
     }
 };
