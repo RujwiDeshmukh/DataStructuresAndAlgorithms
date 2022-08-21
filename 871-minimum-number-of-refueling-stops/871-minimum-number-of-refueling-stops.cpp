7f45923780938ef1d8ec0208e15a154941546c46
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-21 12:46:05 +0530

    Time: 49 ms (51.75%), Space: 16.1 MB (80.69%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
         
-        priority_queue<int> pq;
-        int minStops=0;
-        int currentStationIndex = 0;
-        int totalStations = size(stations);
-        while(startFuel<target)
+        int currentStationIndex=0;
+        int totalStations = stations.size();
+        int minStops = 0;
+        priority_queue<int>pq;
+        
+       // https://leetcode.com/problems/minimum-number-of-refueling-stops/discuss/1266490/Minimum-Number-of-Refueling-Stops-or-Greedy-or-PriorityQueue
+        
+        while(startFuel < target)
         {
-            while(currentStationIndex < totalStations and stations[currentStationIndex][0]<=startFuel)
+     while(currentStationIndex < totalStations && stations[currentStationIndex][0] <= startFuel)
             {
-                pq.push(stations[currentStationIndex][1]);
-                currentStationIndex++;
+              pq.push(stations[currentStationIndex][1]);
+              currentStationIndex++;
             }
+            
             if(pq.empty())
+            {
                 return -1;
-            startFuel+=pq.top();
+            }
+            
+            startFuel += pq.top();
             pq.pop();
             minStops++;
         }
+        
         return minStops;
     }
 };
