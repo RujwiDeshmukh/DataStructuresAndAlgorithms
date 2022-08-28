a324e4af375112bcb31c5bde3018468bb844ad16
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-28 18:40:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (82.86%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,84 +1,37 @@
 class Solution {
 public:
     
-    bool isPowerofTwo(int n)
+    vector<int> countDigits(int n)
     {
+        vector<int>digit(10);
+        //as maximum of 10 digits can be there in the power of 2 
+        //within the range of n which is 10^9
+        //storing freq of each digit in an array
         
-         /*if(n==1)
+        while(n)
         {
-             cout<<n<<endl;
-            return true;
+            digit[n%10]++;
+            n = n/10;
         }
         
-        if(n<1)
-        {
-            return false;
-        }
-        
-        cout<<n<<endl;
-        
-        return n%2 && isPowerofTwo(n/2); */
-        
-        return (n&(n-1))==0;
+        return digit;
     }
-    
-    bool permutations(string str,int left,int right)
-    {
-        if(left>right)
-        {
-            return false;
-        }
-        
-        if(left==right)
-        {
-            char ch = str[0];
-            
-            if(ch != '0' && isPowerofTwo(stoi(str)))
-            {
-                   return true; 
-            }
-            else
-            {
-                return false;
-            }
-        }
-        
-        bool flag;
-        
-        for(int i=left;i<=right;i++)
-        {
-            swap(str[i],str[left]);
-            flag = permutations(str,left+1,right);
-            
-            if(flag)
-            {
-                return true;
-            }
-            
-            swap(str[left],str[i]);
-        }
-        
-        return flag;
-    }
-    
     
     bool reorderedPowerOf2(int n) {
         
-        if(n==1 || n==2)
-        {
-            return true;
-        }
+        vector<int>digits = countDigits(n);
         
-        string str = to_string(n);
-        
-        int size = str.size();
-        
-        if(permutations(str,0,size-1))
-        {
-            return true;
-        }
-        
+         int pow = 1;
+         for(int i=0;i<30;i++)
+         {
+             if(countDigits(pow)==digits)
+             {
+                 return true;
+             }
+             
+             pow = pow<<1;
+         }
+    
         return false;
-        
     }
 };
