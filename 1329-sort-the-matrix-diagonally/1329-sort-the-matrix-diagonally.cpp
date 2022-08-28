b95f95afff3c4294b67a3bdd0cd14ad4904ac844
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-28 17:14:38 +0530

    Time: 20 ms (43.92%), Space: 9.4 MB (28.08%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -5,48 +5,25 @@ public:
         int n = mat.size();
         int m = mat[0].size();
         
-        unordered_map<int,vector<int>> mp;
+        unordered_map<int,priority_queue<int,vector<int>,greater<int>>>mp;
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                mp[i-j].push_back(mat[i][j]);
+                mp[i-j].push(mat[i][j]);
+                //it sort automatically in ascending order do not need to sort it separately
             }
         }
         
-        for(int k=-(m-1);k<n;k++)
-        {
-            sort(mp[k].begin(),mp[k].end());
-        }
-        
-        for(int i=n-1;i>=0;i--) {
-        for(int j=m-1;j>=0;j--) {
-            mat[i][j]=mp[i-j].back();
-            mp[i-j].pop_back();
-        }
-    }
-     
-        //can't use in case of vectors
-      /*  for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 mat[i][j] = mp[i-j].top();
                 mp[i-j].pop();
             }
-        }*/
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                cout<<mat[i][j]<<" ";
-            }
-            
-            cout<<endl;
         }
-        
         
         return mat;
     }
