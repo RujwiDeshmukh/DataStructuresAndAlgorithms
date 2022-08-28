9065ad766d0811a4444b8ca6a3594153552ed43a
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-28 12:26:37 +0530

    Time: 4 ms (38.83%), Space: 8 MB (20.29%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
     
-    int maxMoney(int currentIndex,int n,unordered_map<int,int>& mp,vector<int> & nums)
+    int maxMoney(int ind,vector<int>& nums,vector<int>& dp)
     {
-        if(currentIndex >= n)
+        if(ind==0)
+        {
+            return nums[0];
+        }
+        
+        if(ind<0)
         {
             return 0;
         }
         
-        if(mp.find(currentIndex) != mp.end())
+        if(dp[ind] != -1)
         {
-            return mp[currentIndex];
+            return dp[ind];
         }
         
-        int rob = nums[currentIndex]+maxMoney(currentIndex+2,n,mp,nums);
+        int pick = nums[ind]+maxMoney(ind-2,nums,dp);
+        int notPick = maxMoney(ind-1,nums,dp);
         
-        int notRob = maxMoney(currentIndex+1,n,mp,nums);
-        
-        return mp[currentIndex] = max(rob,notRob);
+        return dp[ind] = max(pick,notPick);
     }
     
-    int rob(vector<int>& nums) {
-        
-        unordered_map<int,int>mp;
+    int rob(vector<int>& nums) { 
         
         if(nums.size()==1)
         {
             return nums[0];
         }
         
-        int zeroIndex = maxMoney(0,nums.size()-1,mp,nums);
-        mp.clear();
-        int firstIndex = maxMoney(1,nums.size(),mp,nums);
-        return max(zeroIndex,firstIndex);
+        vector<int>dp(nums.size(),-1);
+        
+        //here we are not taking into consideration last element
+        //i.e. not including the last element of the array into operation
+        int LastInd = maxMoney(nums.size()-2,nums,dp);
+        //reversing the existing array now tha last element will be the first element
+        //so we are not including the last element of array into operation
+        reverse(nums.begin(),nums.end());
+        
+        fill(dp.begin(),dp.end(),-1);
+        
+        int ZeroInd = maxMoney(nums.size()-2,nums,dp);
+        
+        return max(LastInd,ZeroInd);
     }
 };
