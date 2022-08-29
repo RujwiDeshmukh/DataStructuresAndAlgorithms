989003503f7065c779dc7f025e61deb0378ac269
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-29 22:48:20 +0530

    Time: 7 ms (6.68%), Space: 6.1 MB (59.46%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
-        vector<vector<int>>dp(m, vector<int>(n,-1));
-        
-        //when we reach 0 0 from m-1 n-1 then there will be one path
-        dp[0][0]=1;
+
+        vector<int>prev(n,0);
         
         for(int i=0;i<m;i++)
         {
+            vector<int>curr(n,0);
+            
             for(int j=0;j<n;j++)
             {
                 if(i==0 && j==0)
                 {
-                    dp[i][j]=1;
+                    curr[j]=1;
                 }
                 else
                 {
                     int leftMove =0;
                     if(j>0)
                     {
-                        leftMove = dp[i][j-1];
+                        leftMove = curr[j-1];
                     }
                        
                     int upMove =0;
                     if(i>0)
                     {
-                       upMove = dp[i-1][j];
+                       upMove = prev[j];
                     }
                     
-                    dp[i][j] = leftMove+upMove;
+                    curr[j] = leftMove+upMove;
                 }
                 
-                
             }
-
+            
+            prev =curr;
         }
         
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
