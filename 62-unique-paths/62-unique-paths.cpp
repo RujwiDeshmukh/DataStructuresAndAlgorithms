90557e78aa66840fe8c73bfef83a4f3c1aaba649
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-29 22:21:09 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.35%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
-    
-    int totalWays(int row,int col,int m,int n,vector<vector<int>> & ans)
-    {
-        if(row==0 && col==0)
-        {
-            return 1;
-        }
-        
-        if(row<0 || col<0)
-        {
-           return 0;   
-        }
-        
-        if(ans[row][col] != -1)
-        {
-            return ans[row][col];
-        }
-        
-        int upMove = totalWays(row-1,col,m,n,ans);
-        int leftMove = totalWays(row,col-1,m,n,ans);
-        
-        return ans[row][col] = upMove+leftMove;
-    }
-    
-    
     int uniquePaths(int m, int n) {
-        vector<vector<int>>ans(m,vector<int>(n,-1));
-       return totalWays(m-1,n-1,m,n,ans);
+        
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        
+        //when we reach 0 0 from m-1 n-1 then there will be one path
+        dp[0][0]=1;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                }
+                else
+                {
+                    int leftMove =0;
+                    if(j>0)
+                    {
+                        leftMove = dp[i][j-1];
+                    }
+                       
+                    int upMove =0;
+                    if(i>0)
+                    {
+                       upMove = dp[i-1][j];
+                    }
+                    
+                    dp[i][j] = leftMove+upMove;
+                }
+                
+                
+            }
+
+        }
+        
+        return dp[m-1][n-1];
     }
 };
