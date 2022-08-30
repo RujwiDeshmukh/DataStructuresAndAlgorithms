08cfd13819f7bd8aa99289d8df02811be40e3199
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-30 22:29:39 +0530

    Time: 9 ms (22.37%), Space: 7.9 MB (13.41%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,49 +1,43 @@
 class Solution {
 public:
     
+    int uniquePath(int row,int col,int m,int n,vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp)
+    {
+        
+        if(row==0 && col==0 && obstacleGrid[row][col] != 1)
+        {
+            return 1;
+        }
+         if(row<0 || col<0)
+        {
+            return 0;
+        }
+        
+        if(obstacleGrid[row][col]==1)
+        {
+            return 0;
+        }
+        
+        if(dp[row][col] != -1)
+        {
+            return dp[row][col];
+        }
+        
+        int left = uniquePath(row-1,col,m,n,obstacleGrid,dp);
+        int right = uniquePath(row,col-1,m,n,obstacleGrid,dp);
+        
+        return dp[row][col]=left+right;
+        
+    }
+    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         
-        vector<int>prev(m+1,0);
+        vector<vector<int>>dp(n,vector<int>(m,-1));
         
-        for(int i=0;i<n;i++)
-        {
-            
-             vector<int>temp(m+1,0);
-            
-            for(int j=0;j<m;j++)
-            {
-                
-                if(obstacleGrid[i][j] == 1)
-                {
-                    temp[j] = 0;
-                }
-                else if(i==0 && j==0)
-                {
-                    temp[j]=1;
-                }
-                else 
-                {
-                    int up=0;
-                    int left=0;
-                    if(i>0)
-                    {
-                        up = prev[j];
-                    }
-                    if(j>0)
-                    {
-                        left = temp[j-1];
-                    }
-                        temp[j] = up+left;
-                 }
-                
-                }
-            
-            prev = temp;
-            }
+       return uniquePath(n-1,m-1,n,m,obstacleGrid,dp);
         
-        return prev[m-1];
     }
 };
