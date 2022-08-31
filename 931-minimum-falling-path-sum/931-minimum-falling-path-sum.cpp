82f8db1e1283f5c51c820b819748b85424793f24
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-31 23:37:54 +0530

    Time: 37 ms (10.53%), Space: 10.8 MB (5.11%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -7,39 +7,44 @@ public:
         
         vector<vector<int>>dp(n,vector<int>(n,0));
         
+        vector<int>prev(n,0);
+        
         int mini=99999;
         
         for(int i=0;i<n;i++)
         {
+            vector<int>curr(n,0);
+            
             for(int j=0;j<n;j++)
             {
                 if(i==0)
                 {
-                    dp[i][j]=grid[i][j];
+                    curr[j]=grid[i][j];
                 }
                 else
                 {
-                    int up = grid[i][j]+dp[i-1][j];
+                    int up = grid[i][j]+prev[j];
                     int leftDiag = 999999;
                     if(j>0)
                     {
-                    leftDiag = grid[i][j]+dp[i-1][j-1];
+                    leftDiag = grid[i][j]+prev[j-1];
                     }
                     int rightDiag = 999999;
                     if(j<n-1)
                     {
-                    rightDiag = grid[i][j]+dp[i-1][j+1];
+                    rightDiag = grid[i][j]+prev[j+1];
                     }
                     
-                    dp[i][j] = min(up,min(leftDiag,rightDiag));
+                    curr[j] = min(up,min(leftDiag,rightDiag));
                 }
             }
+            prev=curr;
         }
         
         for(int i=0;i<n;i++)
         {
-             cout<<dp[n-1][i]<<endl; 
-            mini = min(mini,dp[n-1][i]);
+             cout<<prev[i]<<endl; 
+            mini = min(mini,prev[i]);
         }
         
         return mini;
