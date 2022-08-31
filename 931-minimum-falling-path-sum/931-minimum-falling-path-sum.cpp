7c569d34433e178b2f69a70d5a6d1b426a34cc87
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-31 23:33:52 +0530

    Time: 35 ms (13.50%), Space: 10.4 MB (6.19%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,51 +1,45 @@
 class Solution {
 public:
     
-    int minimumSum(int row,int col,int n,vector<vector<int>>& matrix, vector<vector<int>>& dp)
-    {
+    int minFallingPathSum(vector<vector<int>>& grid) {
         
-        if(col>=n || col<0)
-         {
-             return 10000000;
-         }
+        int n = grid.size();
         
-         if(row==0)
-         {
-             return matrix[row][col];
-         }
-        
-         if(dp[row][col] != -1)
-         {
-             return dp[row][col];
-         }
-        
-      /*   if(col>=n || col<0)
-         {
-             return 10000000;
-         }
-         */
-        
-         int up = matrix[row][col]+minimumSum(row-1,col,n,matrix,dp);
-         int leftDiag = matrix[row][col]+minimumSum(row-1,col-1,n,matrix,dp);
-         int rightDiag = matrix[row][col]+minimumSum(row-1,col+1,n,matrix,dp);
-        
-        return dp[row][col] = min(up, min(leftDiag,rightDiag));
-    }
-    
-    
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        int n = matrix.size();
-        
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<int>>dp(n,vector<int>(n,0));
         
         int mini=99999;
         
         for(int i=0;i<n;i++)
         {
-           int ans = minimumSum(n-1,i,n,matrix,dp);
-           
-            mini = min(mini,ans);
+            for(int j=0;j<n;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=grid[i][j];
+                }
+                else
+                {
+                    int up = grid[i][j]+dp[i-1][j];
+                    int leftDiag = 999999;
+                    if(j>0)
+                    {
+                    leftDiag = grid[i][j]+dp[i-1][j-1];
+                    }
+                    int rightDiag = 999999;
+                    if(j<n-1)
+                    {
+                    rightDiag = grid[i][j]+dp[i-1][j+1];
+                    }
+                    
+                    dp[i][j] = min(up,min(leftDiag,rightDiag));
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+             cout<<dp[n-1][i]<<endl; 
+            mini = min(mini,dp[n-1][i]);
         }
         
         return mini;
