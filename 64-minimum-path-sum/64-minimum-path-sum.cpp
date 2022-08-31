16b047c5c4cc4b5710794fc78d51dd47a4a0796f
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-08-31 16:27:12 +0530

    Time: 12 ms (73.53%), Space: 10 MB (65.91%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,66 +1,43 @@
 class Solution {
 public:
-    
-    int minimumPath(int row,int col,int n ,int m, vector<vector<int>>& grid,     vector<vector<int>>& dp)
-    {
-        if(row==0 && col==0)
-        {
-            return grid[row][col];
-        }
-        
-        //we dont want to include this condition in our answer
-        //so we simply return the large value as we want to take the minimum
-        
-        if(row<0 || col<0)
-        {
-            return 9999999;
-        }
-        
-        if(dp[row][col] != -1)
-        {
-            return dp[row][col];
-        }
-        
-        int up = grid[row][col] + minimumPath(row-1,col,n,m,grid,dp);
-        int left = grid[row][col] + minimumPath(row,col-1,n,m,grid,dp);
-        
-        return dp[row][col] = min(up,left);
-    }
-    
-    
+  
     int minPathSum(vector<vector<int>>& grid) {
         
         int n = grid.size();
         int m = grid[0].size();
         
-        vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<int>prev(m,0);
         
         for(int i=0;i<n;i++)
         {
+            vector<int>curr(m,0);
+            
             for(int j=0;j<m;j++)
             {
                 if(i==0 && j==0)
                 {
-                    dp[i][j]=grid[i][j];
+                    curr[j]=grid[i][j];
                 }
                 else
                 {
                     int up = 999999;
                     if(i>0)
                     {
-                      up  = grid[i][j]+ dp[i-1][j];
+                      up  = grid[i][j]+ prev[j];
                     }
                        
                     int left=999999;
                     if(j>0)
                     {
-                       left = grid[i][j] + dp[i][j-1];
+                       left = grid[i][j] + curr[j-1];
                     }
-                    dp[i][j] = min(up,left);
-                }
+                    curr[j] = min(up,left);
+                } 
             }
+            
+            prev = curr;
         }
         
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
