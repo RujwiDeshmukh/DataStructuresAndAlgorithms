aa2a039fae119ba08656ddd04289ca0a17d14dc8
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-03 13:18:34 +0530

    Time: 3 ms (74.35%), Space: 7.2 MB (82.84%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,46 +1,48 @@
 class Solution {
 public:
-    
-    int countDig(int digit)
-    {
-        int cnt=0;
-        
-        while(digit > 0)
-        {
-            digit = digit/10;
-            cnt++;
-        }
-        
-        return cnt;
-    }
-    
-    void dfs(int curr_digit,int n,int k,vector<int> & ans)
-    {
-        if(countDig(curr_digit)==n)
-        {
-            ans.push_back(curr_digit);
-            return ;
-        }
-        
-        for(int i=0;i<10;i++)
-        {
-            int last_digit = curr_digit%10;
-            if(abs(last_digit-i)==k)
-            {
-                dfs(curr_digit*10+i,n,k,ans);
-            }
-        }
-    }
-    
+    //https://leetcode.com/problems/numbers-with-same-consecutive-differences/discuss/2521519/LeetCode-The-Hard-Way-Easy-BFS-100-Explained-Line-By-Line
     vector<int> numsSameConsecDiff(int n, int k) {
         
-         vector<int>ans;
+       // queue<int>q {1,2,3,4,5,6,7,8,9};
+        queue<int>q;
+        q.push(1);q.push(2);q.push(3);q.push(4);q.push(5);q.push(6);
+        q.push(7);q.push(8);q.push(9);
         
-          for(int i=1;i<10;i++)
-          {
-              dfs(i,n,k,ans);
-          }
+        while(n > 1)  //the loop will run only for n-1 times , as already 1 digit 
+            //there in queue
+        {
+            int size=q.size();
+            
+            for(int i=0;i<size;i++)
+            {
+                int num = q.front();
+                q.pop();
+                
+                int last_dig = num%10;
+                
+                for(int i=0;i<10;i++)
+                {
+                    if(abs(last_dig-i)==k)
+                    {
+                        q.push(num*10+i);
+                    }
+                }
+            }
+            
+            n--;
+        }
         
-          return ans;
+       // vector<int>ans {q.begin(),q.end()};
+        vector<int>ans;
+        
+        while(!q.empty())
+        {
+            int num = q.front();
+            q.pop();
+            
+            ans.push_back(num);
+        }
+        
+        return ans;
     }
 };
