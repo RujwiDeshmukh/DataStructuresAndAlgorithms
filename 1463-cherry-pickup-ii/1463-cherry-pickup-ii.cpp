04f98c412073398e3e18d478bb83961876a7ca7a
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-06 17:41:40 +0530

    Time: 278 ms (26.31%), Space: 18.6 MB (6.94%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -60,12 +60,67 @@ public:
         
        // https://leetcode.com/problems/cherry-pickup-ii/discuss/1674294/C%2B%2B-oror-RECURSION-%2B-MEMOIZATION-oror-FULLY-EXPLAINED-oror-4-D-DP-oror-3-D-DP
         
-        memset(dp,-1,sizeof(dp));;
-        
         int n = grid.size();
         int m = grid[0].size();
         
-        return maxCherries(0,0,m-1,n,m,grid);
+       vector<vector<vector<int>>>dp(n,vector<vector<int>>(m, vector<int>(m,0)));
         
+       // return maxCherries(0,0,m-1,n,m,grid);
+        
+        //write base case first
+        
+        for(int j1=0;j1<m;j1++)
+        {
+            for(int j2=0;j2<m;j2++)
+            {
+                if(j1==j2)
+                {
+                    dp[n-1][j1][j2] = grid[n-1][j1];
+                }
+                else
+                {
+                    dp[n-1][j1][j2] = grid[n-1][j1]+grid[n-1][j2];
+                }
+            }
+        }
+        
+        //as base case is n-1 so we will compute from n-2
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j1=0;j1<m;j1++)
+            {
+                for(int j2=0;j2<m;j2++)
+                {
+                    int cherry=0;
+                    
+                    if(j1==j2)
+                    {
+                        cherry = grid[i][j1];
+                    }
+                    else
+                    {
+                        cherry = grid[i][j1]+grid[i][j2];
+                    }
+                    
+                   int maxi=INT_MIN;
+                    
+                     for(int c1=-1;c1<=1;c1++)
+                  {
+                         for(int c2=-1;c2<=1;c2++)
+                        {
+                             if(j1+c1 >= 0 && j1+c1 < m && j2+c2 >= 0 && j2+c2< m)
+                             {
+                                 maxi=max(maxi,dp[i+1][j1+c1][j2+c2]);
+                             }
+                       }
+                  }  
+                    
+                    dp[i][j1][j2]=maxi+cherry;
+                }
+            }
+        }
+        
+        return dp[0][0][m-1];
     }
 };
