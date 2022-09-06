32f14fa1be82d10b6d475692506cea4836ba4754
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-06 22:16:23 +0530

    Time: 12 ms (39.16%), Space: 9 MB (52.59%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -25,8 +25,38 @@ public:
         
         int n = triangle.size();
         
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<int>>dp(n,vector<int>(n,0));
         
-        return minimumSum(0,0,triangle,dp);
+       // return minimumSum(0,0,triangle,dp);
+        
+        //base condition it is 
+        //as base condition is computed for row n-1
+        //so we have to start for loop from n-2
+        
+        for(int j=0;j<triangle[n-1].size();j++)
+        {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int row=n-2;row>=0;row--)
+        {
+            for(int col=0;col<triangle[row].size();col++)
+            {
+                int down = INT_MAX;
+                int diag = INT_MAX;
+                if(row < n-1)
+                {
+                    down = triangle[row][col]+dp[row+1][col];
+                }
+                
+                if(row < n-1)
+                {
+                    diag = triangle[row][col]+dp[row+1][col+1];
+                }
+               
+                dp[row][col] = min(down,diag);
+            }
+        }
+        return dp[0][0];
     }
 };
