afb2d101d839554a9fdf441de0de90efe7400b42
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-06 22:30:03 +0530

    Time: 31 ms (6.25%), Space: 9.4 MB (5.20%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
     
-    int minimumSum(int row,int col,vector<vector<int>>& triangle,vector<vector<int>>& dp)
-    {
-        if(row==triangle.size()-1)
-        {
-            return triangle[row][col];
-        }
-        
-        if(dp[row][col] != -1)
-        {
-            return dp[row][col];
-        }
-        
-        //for diagonal condition it wont cross as we are stopping for row==n-1
-        
-        int down = triangle[row][col]+minimumSum(row+1,col,triangle,dp);
-        int diag = triangle[row][col]+minimumSum(row+1,col+1,triangle,dp);
-        
-        return dp[row][col] = min(down,diag);
-    }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n = triangle.size();
         
         vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<int>front(n,0);
         
        // return minimumSum(0,0,triangle,dp);
         
@@ -35,28 +16,33 @@ public:
         
         for(int j=0;j<triangle[n-1].size();j++)
         {
-            dp[n-1][j] = triangle[n-1][j];
+            front[j] = triangle[n-1][j];
         }
         
         for(int row=n-2;row>=0;row--)
         {
+            vector<int>curr(n,0);
+               
             for(int col=0;col<triangle[row].size();col++)
             {
                 int down = INT_MAX;
                 int diag = INT_MAX;
                 if(row < n-1)
                 {
-                    down = triangle[row][col]+dp[row+1][col];
+                    down = triangle[row][col]+front[col];
                 }
                 
                 if(row < n-1)
                 {
-                    diag = triangle[row][col]+dp[row+1][col+1];
+                    diag = triangle[row][col]+front[col+1];
                 }
                
-                dp[row][col] = min(down,diag);
+                curr[col] = min(down,diag);
             }
+            
+            front = curr;
         }
-        return dp[0][0];
+        
+        return front[0];
     }
 };
