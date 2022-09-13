555c5263e2d67151eee4921a745352c9820912e1
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-13 22:00:42 +0530

    Time: 27 ms (28.68%), Space: 14 MB (38.65%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        
+        int remaining=0;
+        for(int i=0;i<data.size();i++)
+        {
+            int x = data[i];
+            
+            if(remaining==0)
+            {
+                if((x>>5)==0b110)
+                {
+                    remaining=1;
+                }
+            
+               else if((x>>4)==0b1110)
+               {
+                  remaining=2;
+               }
+                else if((x>>3)==0b11110)
+                {
+                    remaining=3;
+                }
+                else if((x>>7)!=0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if((x>>6)!=0b10)
+                {
+                    return false;
+                }
+                else
+                {
+                    remaining--;
+                }
+            }
+            
+        }
+        
+        return remaining==0;
+        
+    }
+};
