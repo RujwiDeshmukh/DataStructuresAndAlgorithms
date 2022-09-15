6c5e554b6fa65b40ce514f3be152a549550c380a
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-15 19:29:43 +0530

    Time: 444 ms (88.56%), Space: 146.3 MB (38.40%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -6,24 +6,36 @@ public:
         {
             return {};
         }
-        vector<int>ans;
-        sort(changed.begin(),changed.end());
+        vector<int>ans;      
+        vector<int>uniqueNums;
         unordered_map<int,int>mp;
         
-        for(int i=0;i<n;i++)
-        {
-            mp[changed[i]]++;
-        }
+        for(int i=0;i<n;i++) {  mp[changed[i]]++;  }
         
-        for(int i=0;i<n;i++)
-        {
-            if(mp[changed[i]]==0) continue;
-            if(mp[2*changed[i]]==0) return {};
-            ans.push_back(changed[i]);
-            mp[changed[i]]--;
-            mp[2*changed[i]]--;
-        }
+        //just to eliminate repeated elements from array
+        //to decrease the sorting Time Complexity
+        
+        for(auto m : mp)   {  uniqueNums.push_back(m.first);   }
+        
+        sort(uniqueNums.begin(),uniqueNums.end());     
+        
+       for(auto num : uniqueNums)
+       {
+           if(mp[num] > mp[2*num]) {  return {}; };
+           
+           for(int i=0;i<mp[num];i++)
+           {
+               ans.push_back(num);
+               mp[2*num]--;
+           }
+       }
         
         return ans;
+        
+        //https://leetcode.com/problems/find-original-array-from-doubled-array/discuss/2577700/C%2B%2B-or-HashmapSorting(NlogN)-Approach
+        
+        //https://leetcode.com/problems/find-original-array-from-doubled-array/discuss/1470959/JavaC%2B%2BPython-Match-from-the-Smallest-or-Biggest-100
+        
+        //https://leetcode.com/problems/find-original-array-from-doubled-array/discuss/2577514/LeetCode-The-Hard-Way-Explained-Line-By-Line
     }
 };
