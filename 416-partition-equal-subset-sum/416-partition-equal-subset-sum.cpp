856c8ce09a47c22292c4fdfd9a50d75d91e7af87
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-15 22:36:25 +0530

    Time: 510 ms (30.23%), Space: 12.4 MB (65.74%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -22,38 +22,44 @@ public:
         
         int target = totSum/2;
         
-        vector<vector<bool>>dp(n+5,vector<bool>(target+5,false));
+        //vector<vector<bool>>dp(n+5,vector<bool>(target+5,false));
+        vector<bool>prev(target+1,false);
         
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0]=true;
-        }
+        //for target zero everytime it will be true
+        
+        prev[0] = true;
         
         //as target is 50 and our first element here is 99 so
         //it will give the error so every time target should be 
         //equal to nums[0] or always greatre than zero
         if(target >= nums[0])
         {
-            dp[0][nums[0]]=true;
+            prev[nums[0]]=true;
         }
         
         for(int i=1;i<n;i++)
         {
+            //number of columns will be equal to target value
+            //so the size of curr will be equal to target only
+            vector<bool>curr(target+1,false);
+               
             for(int j=1;j<=target;j++)
             {
-                bool notTake = dp[i-1][j];
+                bool notTake = prev[j];
                 bool take = false;
                 
                 if(nums[i] <= j)
                 {
-                    take = dp[i-1][j-nums[i]];
+                    take = prev[j-nums[i]];
                 }
                 
-                dp[i][j] = take | notTake;
+                curr[j] = take | notTake;
             }
+            
+            prev = curr;
         }
         
-        return dp[n-1][target];
+        return prev[target];
         
     }
 };
