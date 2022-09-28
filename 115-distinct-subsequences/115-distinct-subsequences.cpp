fa876fd1915d2b6ec3b58bbb70f96f7eb7cbbe95
Rujwi Deshmukh <rjdeshmukh2000@gmail.com>
2022-09-28 22:27:54 +0530

    Time: 80 ms (31.64%), Space: 30.1 MB (16.54%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    
-    int distinctSub(string s,string t,int i,int j,vector<vector<int>>& dp)
-    {
-        if(j<0) return 1;
-        if(i<0) return 0;
-        
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        
-        if(s[i]==t[j])
-        {
-            return dp[i][j] = distinctSub(s,t,i-1,j-1,dp)+distinctSub(s,t,i-1,j,dp);
-            //first consideration is match and reduce both
-            //second consideration is match and search for another match
-        }
-        
-        return dp[i][j] = distinctSub(s,t,i-1,j,dp);
-    }
-    
     int numDistinct(string s, string t) {
         
         int n = s.size();
         int m = t.size();
         
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
         
-        return distinctSub(s,t,n-1,m-1,dp);
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return (int)dp[n][m];
     }
 };
